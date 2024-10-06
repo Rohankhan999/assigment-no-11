ce17a49c0e407a5e9b76253e1bac577fef56acaf
ohan <159823468+Rohankhan999@users.noreply.github.com>
2024-10-06 11:35:40 +0500

    ..

---
+++ app.js
@@ -0,0 +1,7 @@
+// Question no 01//
+
+// var num = 3.45214;
+// var result =(+Math.round(num));
+// var result2 =(Math.round(num));
+// var result3 =(Math.round(num));
+// document.write("<h1>"+ "Number : " + num + "<br>"+ "Round off : " + result + "<br>" + "Floor value : " + result2 + "<br>" + "Ceil value : " + result3)
